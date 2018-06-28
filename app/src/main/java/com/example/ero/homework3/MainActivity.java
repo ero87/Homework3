702129f610d2165b956ce0c43d34eb1b1dd11a0c
ero87 <ero-87@mail.ru>
2018-06-29 03:12:29 +0400

    das3

---
+++ app/src/main/java/com/example/ero/homework3/MainActivity.java
@@ -0,0 +1,69 @@
+package com.example.ero.homework3;
+
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.net.Uri;
+import android.os.Bundle;
+import android.provider.MediaStore;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import com.squareup.picasso.Picasso;
+import java.io.IOException;
+import java.util.AbstractSequentialList;
+
+public class MainActivity extends AppCompatActivity {
+
+    private static final int PICK_IMAGE = 100;
+    private int REQUEST_CODE = 3;
+    Uri imageUri;
+
+    String ImageUrl = "https://t-l.ru/i/n/807/240807/240807_b789bdb65021.jpg";
+    private ImageView imageView1;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        final Button button1 = findViewById(R.id.button1);
+        final Button button2 = findViewById(R.id.button2);
+        final Button button3 = findViewById(R.id.button3);
+        imageView1 = findViewById(R.id.a7);
+
+        button1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                imageView1.setImageResource(R.drawable.a777);
+            }
+        });
+
+        button2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Picasso.get().load(ImageUrl).fit().into(imageView1);
+            }
+        });
+
+        button3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+              openGallery();
+            }
+        });
+    }
+
+    public void openGallery(){
+        Intent intent = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.INTERNAL_CONTENT_URI);
+        startActivityForResult(intent, PICK_IMAGE);
+    }
+        @Override
+        protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+            super.onActivityResult(requestCode, resultCode, data);
+            if(requestCode == RESULT_OK && requestCode == PICK_IMAGE){
+               imageUri = data.getData();
+               imageView1.setImageURI(imageUri);
+            }
+        }
+}
